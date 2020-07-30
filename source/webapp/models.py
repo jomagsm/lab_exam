5e831400d165dd66a67be290afa7dd1f3f9e3625
jomagsm <jomagsm@gmail.com>
2020-07-30 20:01:25 +0600

    Зделал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+DEFAUL_CATEGORY = 'other'
+CATEGORY_CHOICE = [(DEFAUL_CATEGORY, 'Разное'), ('food', 'Продукты питания'),
+                   ('household', 'Хоз.товары'), ('toys', 'Детские игрушки'),
+                   ('electronics', 'Электроника')]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    category = models.CharField(max_length=20, verbose_name='Категория', choices=CATEGORY_CHOICE,
+                                default=DEFAUL_CATEGORY)
+    amount = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return "{}. {}".format(self.name, self.amount)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
