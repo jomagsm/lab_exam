9f49e52e3934641f1d320ff88e1f6c9cb8f0c769
jomagsm <jomagsm@gmail.com>
2020-07-30 23:13:22 +0600

    Добавил создание нового товара

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view, view_product
+from webapp.views import index_view, view_product, product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
-    path('view/<int:pk>/', view_product, name='view')
+    path('view/<int:pk>/', view_product, name='view'),
+    path('product_create/', product_create_view, name='product_create')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -10,12 +10,12 @@ from webapp.models import Product
 def index_view(request):
     is_admin = request.GET.get('is_admin', None)
     if is_admin:
-        data = Product.objects.all()
+        order_date = Product.objects.all()
     else:
         data = Product.objects.filter(amount__gt=1)
-        data.order_by('category', 'name')
+        order_date = data.order_by('category', 'name')
     return render(request, 'index.html', context={
-        'products': data
+        'products': order_date
     })
 
 
@@ -23,3 +23,28 @@ def view_product(request, pk):
     data = get_object_or_404(Product, pk=pk)
     context = {'product': data}
     return render(request, 'product_view.html', context)
+
+
+def product_create_view(request):
+    if request.method == "GET":
+        return render(request, 'product_create.html', context={
+            'form': ProductForm()
+        })
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            # article = Article.objects.create(**form.cleaned_data)
+            product = Product.objects.create(
+                name=form.cleaned_data['name'],
+                description=form.cleaned_data['description'],
+                category=form.cleaned_data['category'],
+                amount=form.cleaned_data['amount'],
+                price=form.cleaned_data['price']
+            )
+            return redirect('view', pk=product.pk)
+        else:
+            return render(request, 'product_create.html', context={
+                'form': form
+            })
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
